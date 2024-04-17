da0d2762ea4505f972248012ffecc627646158bb
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-04-17 12:00:44 -0400

    Add convenience imports

--- src/mednewsqa/medicalxpress/__init__.py
+++ src/mednewsqa/medicalxpress/__init__.py
@@ -1,2 +1,2 @@
-from .article import get_article_text
-from .search import get_search_results
+from .article import get_article_text  # noqa: F401
+from .search import get_search_results  # noqa: F401
