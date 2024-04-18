e0c2dd348f016866efe5e91dbceffd3fec1df70d
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-04-17 22:18:10 -0400

    Improve heading tag replacement

--- src/mednewsqa/medicalxpress/article.py
+++ src/mednewsqa/medicalxpress/article.py
@@ -72,17 +72,13 @@ def _get_article_content(url: str) -> list[str]:
 
     # Replace heading tags with paragraph tag
     # This is done because the hext rule extracts text from p tags only.
-    heading_level = 1
-    while True:
+    for heading_level in range(1, 7):
         heading_tag_half_open, heading_tag_full_open, heading_tag_close = f"<h{heading_level} ", f"<h{heading_level}>", f"</h{heading_level}>"
         if heading_tag_close in html:
             html = html.replace(heading_tag_half_open, "<p ")
             html = html.replace(heading_tag_full_open, "<p>")
             html = html.replace(heading_tag_close, "</p>")
             # print(f"Replaced h{heading_level} with p tags.")
-            heading_level += 1
-        else:
-            break
 
     rule = _HEXT
     html = hext.Html(html)
