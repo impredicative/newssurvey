2d860dd8674bc372c07cd7607f8225e57aa2fb3f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-04-17 21:41:42 -0400

    Update content filters

--- src/mednewsqa/medicalxpress/article.py
+++ src/mednewsqa/medicalxpress/article.py
@@ -37,11 +37,14 @@ _MID_CONTENT_BLACKLIST = {
     "proofread",
     "reputable news agency",
 }
-_MID_CONTENT_STARTSWITH_BLACKLIST = ("This article has been reviewed ",)
+_MID_CONTENT_STARTSWITH_BLACKLIST = (
+    "This article has been reviewed ",
+    "©",
+)
 _POST_CONTENT_BLACKLIST = {
     "Explore further",
 }
-_POST_CONTENT_STARTSWITH_BLACKLIST = ("©",)
+_POST_CONTENT_STARTSWITH_BLACKLIST = ()
 # _CONTENT_TRAILING_RE_BLACKLISTED = re.compile(r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d{1,2}), (\d{4})$")
 
 
