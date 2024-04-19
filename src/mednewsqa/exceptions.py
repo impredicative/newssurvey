b8d901dfa00448939def69acde3dd1a7df069f30
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-04-19 11:49:02 -0400

    Define exceptions

--- src/mednewsqa/exceptions.py
+++ src/mednewsqa/exceptions.py
@@ -2,5 +2,21 @@ class Error(Exception):
     """Base error."""
 
 
+class EnvError(Error, EnvironmentError):
+    """Environment variable availability error."""
+
+
+class InputError(Error, ValueError):
+    """Input validity error."""
+
+
+class ModelOutputError(Error):
+    """Model output error."""
+
+
+class LanguageModelOutputError(ModelOutputError):
+    """Language model output error."""
+
+
 class RequestError(Error):
     """Malformed HTTP request error"""
