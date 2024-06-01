8e758b259211ac1789a54151adc35e9690d9071e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-06-01 15:28:54 -0400

    Update get_query to allow abort

--- src/newsqa/workflow/user/query.py
+++ src/newsqa/workflow/user/query.py
@@ -43,6 +43,8 @@ def get_query(*, source_type: str, approach: str = "click.edit") -> str:
     """Get user query from user input.
 
     `source_type` is a string that corresponds to the SOURCE_TYPE variable of the corresponding source.
+
+    `InputError` is raised if the approach is "click.edit" and the user-supplied query is empty.
     """
     query = None
     while not query:
@@ -52,6 +54,8 @@ def get_query(*, source_type: str, approach: str = "click.edit") -> str:
             case "click.edit":
                 query = click.edit(text=f"\n# Specify the {source_type} question or concern in one or more lines.\n# Lines starting with # will be skipped.", require_save=False) or ""
                 query = "\n".join(ln for ln in query.splitlines() if not ln.lstrip().startswith("#"))
+                if not query.strip():
+                    raise newsqa.exceptions.InputError("No query was provided.")  # Note: This is the only way for the user to abort the editor.
             case _:
                 assert False, approach
         query = query.strip()
