ca74eb5e77b4cbcc44b77982eed5eadb493825af
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-06-03 01:45:32 -0400

    Update medicalxpress cooldown period to 3s

--- src/newsqa/sources/medicalxpress/_common.py
+++ src/newsqa/sources/medicalxpress/_common.py
@@ -4,4 +4,4 @@ SOURCE_SITE = "medicalxpress.com"
 SOURCE_SITE_NAME = "MedicalXpress"
 SOURCE_TYPE = "medical"
 
-request_cooldown_lock = CooldownLock(cooldown=0.5, name=SOURCE_SITE_NAME)
+request_cooldown_lock = CooldownLock(cooldown=3, name=SOURCE_SITE_NAME)  # Note: 2s or lower values of cooldown led to HTTP 429 error.
