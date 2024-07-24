59e9890466ca01962d5e32fbdfaab7b83908e140
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-24 20:15:49 +0000

    Update openai utility module to support both large and small models

--- src/newsqa/util/openai_.py
+++ src/newsqa/util/openai_.py
@@ -17,7 +17,10 @@ _COLOR_RESET = "\033[0m"
 
 _DISKCACHE = get_diskcache(__file__, size_gib=10)
 MODELS = {  # Ref: https://platform.openai.com/docs/models/
-    "text": ["gpt-4o-2024-05-13", "gpt-4-turbo-2024-04-09"][0],
+    "text": {
+        "large": "gpt-4o-2024-05-13",
+        "small": "gpt-4o-mini-2024-07-18",
+    },
     "embeddings": "text-embedding-3-large",
 }
 
@@ -31,18 +34,21 @@ def ensure_openai_key() -> None:
 @_DISKCACHE.memoize(expire=datetime.timedelta(weeks=52).total_seconds(), tag="get_completion")
 def get_completion(prompt: str, model: str) -> ChatCompletion:  # Note: `model` is explicitly specified to allow model-specific caching.
     """Return the completion for the given prompt."""
+    assert model in MODELS["text"].values(), model
     client = openai.OpenAI()
-    print(f"Requesting completion for prompt of length {len(prompt)}.")
+    print(f"Requesting completion for prompt of length {len(prompt)} using model {model}.")
     completion = client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt}])
-    print(f"Received completion for prompt of length {len(prompt)}.")
+    print(f"Received completion for prompt of length {len(prompt)} using model {model}.")
     # Note: Specifying max_tokens=4096 with gpt-4-turbo-preview did not benefit in increasing output length, and a higher value is disallowed. Ref: https://platform.openai.com/docs/api-reference/chat/create
     return completion
 
 
-def get_content(prompt: str, *, completion: Optional[ChatCompletion] = None, log: bool = True) -> str:
+def get_content(prompt: str, *, model_size: str = "large", completion: Optional[ChatCompletion] = None, log: bool = True) -> str:
     """Return the completion content for the given prompt."""
+    assert model_size in MODELS["text"], model_size
+    model = MODELS["text"][model_size]
     if not completion:
-        completion = get_completion(prompt, model=MODELS["text"])
+        completion = get_completion(prompt, model=model)
     content = completion.choices[0].message.content
     content = content.strip()
     assert content
