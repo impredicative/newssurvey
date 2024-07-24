0ad7b4c94503eef428d67eacf809d8d854ee398c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-24 20:10:13 +0000

    Update generate_response function docstring

--- src/newsqa/newsqa.py
+++ src/newsqa/newsqa.py
@@ -15,10 +15,10 @@ def generate_response(source: str, query: str, output_path: Optional[Path] = Non
     The progress is printed to stdout.
 
     Params:
-    * `source` (-s): Name of supported news source.
-    * `query` (-q): Question or concern answerable by the news source.
-    * `path (-p)`: Output file path. If given, the response is also written to this text file except if there is an error.
-    * `confirm` (-c): Confirm as the workflow progresses. If true, a confirmation is interactively sought as each step of the workflow progresses. Its default is false.
+    * `source`: Name of supported news source.
+    * `query`: Question or concern answerable by the news source.
+    * `path`: Output file path. If given, the response is also written to this text file.
+    * `confirm`: Confirm as the workflow progresses. If true, a confirmation is interactively sought as each step of the workflow progresses. Its default is false.
 
     If failed, a subclass of the `newsqa.exceptions.Error` exception is raised.
     """
@@ -32,7 +32,7 @@ def generate_response(source: str, query: str, output_path: Optional[Path] = Non
     query_sep = "\n" if (len(query.splitlines()) > 1) else " "
     print(f"QUERY:{query_sep}{query}")
 
-    print(f"MODELS: text={MODELS["text"]}, embeddings={MODELS["embeddings"]}")
+    print(f"MODELS: large={MODELS["text"]["large"]}, small={MODELS["text"]["small"]}, embeddings={MODELS["embeddings"]}")
 
     search_terms = list_search_terms(user_query=query, source_module=source_module)
     print(f"SEARCH TERMS ({len(search_terms)}): " + ", ".join(search_terms))
