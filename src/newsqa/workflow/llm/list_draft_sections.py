ef8a156246148671b87b81fa639e9cb7f74c8189
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-25 19:17:44 +0000

    Update _list_draft_sections_for_search_result function to improve assertion about article title

--- src/newsqa/workflow/llm/list_draft_sections.py
+++ src/newsqa/workflow/llm/list_draft_sections.py
@@ -45,8 +45,8 @@ def _list_draft_sections_for_search_result(user_query: str, source_module: Modul
     assert user_query
 
     article_text = source_module.get_article_text(search_result["link"])
-    assert article_text.startswith(search_result["title"]), (search_result["title"], article_text)  # If this fails, fix the parsing, otherwise conditionally prepend the title.
     article = SearchArticle(**search_result, text=article_text)
+    assert article_text.startswith(search_result["title"]), article  # If this fails, fix the parsing, otherwise conditionally prepend the title.
 
     prompt_data = {"user_query": user_query, "source_site_name": source_module.SOURCE_SITE_NAME, "source_type": source_module.SOURCE_TYPE}
     prompt_data["task"] = PROMPTS["3. list_draft_sections"].format(**prompt_data, article=article_text)
