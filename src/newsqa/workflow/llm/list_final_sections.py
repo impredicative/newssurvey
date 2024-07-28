3b1ed2361b415c339b0a783617cccded9a59c8e0
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-28 03:57:32 +0000

    Update `_list_final_sections_for_sample` implementation to use OpenAI API for generating final section names

--- src/newsqa/workflow/llm/list_final_sections.py
+++ src/newsqa/workflow/llm/list_final_sections.py
@@ -1,8 +1,14 @@
 import copy
+import contextlib
+import io
 import random
 from types import ModuleType
 
+from newsqa.exceptions import LanguageModelOutputStructureError
+from newsqa.config import PROMPTS
 from newsqa.types import AnalyzedArticle
+from newsqa.util.openai_ import get_content
+from newsqa.util.sys_ import print_error
 
 
 def _list_final_sections_for_sample(user_query: str, source_module: ModuleType, draft_sections: list[str]) -> dict[str, str]:
@@ -11,7 +17,17 @@ def _list_final_sections_for_sample(user_query: str, source_module: ModuleType, 
     `LanguageModelOutputError` is raised if the model output has an error.
     Specifically, its subclass `LanguageModelOutputStructureError` is raised if the output is structurally invalid.
     """
-    return {draft_section: draft_section.title() for draft_section in draft_sections}  # Placeholder.
+    # return {draft_section: draft_section.title() for draft_section in draft_sections}  # Placeholder.
+    assert user_query
+    assert draft_sections
+
+    prompt_data = {"user_query": user_query, "source_site_name": source_module.SOURCE_SITE_NAME, "source_type": source_module.SOURCE_TYPE}
+    numbered_draft_sections = "\n".join(f"{i}. {s}" for i, s in enumerate(draft_sections, start=1))
+    prompt_data["task"] = PROMPTS["4. list_final_sections"].format(**prompt_data, draft_sections=numbered_draft_sections)
+    prompt = PROMPTS["0. common"].format(**prompt_data)
+    response = get_content(prompt, model_size="small", log=True)
+    
+    assert False  # incomplete
 
 
 def list_final_sections(user_query: str, source_module: ModuleType, articles_and_draft_sections: list[AnalyzedArticle]) -> list[AnalyzedArticle]:
@@ -22,7 +38,7 @@ def list_final_sections(user_query: str, source_module: ModuleType, articles_and
     """
     articles_and_sections = copy.deepcopy(articles_and_draft_sections)
     del articles_and_draft_sections  # Note: This prevents accidental modification of draft sections.
-    max_section_sample_size = 100
+    max_section_sample_size = 500  # Note: Using 1000 was observed to lead to a premature truncation of the response by the model after the first 800.
     votes_needed_to_finalize_section = {"test": 1, "qa": 2, "prod": 3}["prod"]
     rng = random.Random(0)
 
