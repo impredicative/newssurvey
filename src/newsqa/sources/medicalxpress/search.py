e2fbbedceacd4f3b9e2f1b41eb9021ff5f14d79b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-31 04:54:18 +0000

    Update `_get_search_response` memoization expiration to 4 weeks

--- src/newsqa/sources/medicalxpress/search.py
+++ src/newsqa/sources/medicalxpress/search.py
@@ -26,7 +26,7 @@ class UnsupportedPageError(RequestError):
     """Excessive page number request error."""
 
 
-@_DISKCACHE.memoize(expire=datetime.timedelta(weeks=1).total_seconds(), tag="_get_search_response")
+@_DISKCACHE.memoize(expire=datetime.timedelta(weeks=4).total_seconds(), tag="_get_search_response")
 def _get_search_response(query: str, *, sort_by: str = "relevancy", headlines: bool = False, page_num: int = 1) -> requests.Response:
     """Return a response from the MedicalXpress website for a given query, sorting preference, and page number.
 
