448360956e6ef7b343fe8f5132bbd4113ef53b33
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-07-31 19:01:57 +0000

    Update list_final_sections function to use simpler iteration and convergence

--- src/newsqa/workflow/llm/list_final_sections.py
+++ src/newsqa/workflow/llm/list_final_sections.py
@@ -91,6 +91,8 @@ def _list_final_sections_for_sample(user_query: str, source_module: ModuleType, 
 
     Any draft section names that the model abstains from providing final section names for are skipped from the returned mapping.
 
+    The order of draft sections in the returned mapping is not guaranteed to be the same as the input order. This is due to conditional use of order randomization.
+
     `LanguageModelOutputError` is raised if the model output has an error.
     Specifically, its subclass `LanguageModelOutputStructureError` is raised if the output is structurally invalid.
     """
@@ -98,14 +100,20 @@ def _list_final_sections_for_sample(user_query: str, source_module: ModuleType, 
     assert user_query
     assert draft_sections
 
-    prompt_data = {"user_query": user_query, "source_site_name": source_module.SOURCE_SITE_NAME, "source_type": source_module.SOURCE_TYPE}
-    numbered_draft_sections = [f"{i}. {s}" for i, s in enumerate(draft_sections, start=1)]
-    numbered_draft_sections_str = "\n".join(numbered_draft_sections)
-    prompt_data["task"] = PROMPTS["4. list_final_sections"].format(**prompt_data, draft_sections=numbered_draft_sections_str)
-    prompt = PROMPTS["0. common"].format(**prompt_data)
+    rng = random.Random(0)
+    prompt_source_data = {"user_query": user_query, "source_site_name": source_module.SOURCE_SITE_NAME, "source_type": source_module.SOURCE_TYPE}
 
     for num_attempt in range(1, max_attempts + 1):
-        response = get_content(prompt, model_size="small", log=True, read_cache=num_attempt == 1)
+        if num_attempt > 1:
+            rng.shuffle(draft_sections)  # Note: This is done to try to prevent the model from repeatedly failing on a fixed order of draft sections as has been observed.
+
+        numbered_draft_sections = [f"{i}. {s}" for i, s in enumerate(draft_sections, start=1)]
+        numbered_draft_sections_str = "\n".join(numbered_draft_sections)
+        prompt_data = copy.deepcopy(prompt_source_data)
+        prompt_data["task"] = PROMPTS["4. list_final_sections"].format(**prompt_source_data, draft_sections=numbered_draft_sections_str)
+        prompt = PROMPTS["0. common"].format(**prompt_data)
+
+        response = get_content(prompt, model_size="small", log=False)
 
         numbered_response_sections = [line.strip() for line in response.splitlines()]
         numbered_response_sections = [line for line in numbered_response_sections if line]
@@ -131,9 +139,9 @@ def _list_final_sections_for_sample(user_query: str, source_module: ModuleType, 
     if abstained_draft_sections:
         # Note: This feature is implemented to discourage the model from emitting an invalid value for the final section name, e.g. "Not Applicable", as had otherwise been observed.
         num_draft_sections, num_abstained_draft_sections = len(draft_to_final_sections), len(abstained_draft_sections)
-        msg = f"The model abstained from providing final section names for {num_abstained_draft_sections}/{num_draft_sections} draft sections names: {", ".join(abstained_draft_sections)}"
+        msg = f"The model abstained from providing final section names for {num_abstained_draft_sections}/{num_draft_sections} draft sections names."
         if num_abstained_draft_sections == num_draft_sections:
-            raise LanguageModelOutputStructureError(msg)
+            raise LanguageModelOutputStructureError(msg)  # Note: If this gets observed, it can perhaps be handled as a failed attempt by checking for it in `_are_sections_valid`.
         print(msg)
         for abstained_draft_section in abstained_draft_sections:
             assert draft_to_final_sections[abstained_draft_section].lower() == _ABSTAINED_FINAL_SECTION_NAME
@@ -154,44 +162,35 @@ def list_final_sections(user_query: str, source_module: ModuleType, articles_and
     articles_and_sections = copy.deepcopy(articles_and_draft_sections)
     del articles_and_draft_sections  # Note: This prevents accidental modification of draft sections.
 
-    votes_needed_to_finalize_section = 1  # Note: Using a value >1, e.g. 2, whether initially or incrementally, led to extremely slow progress with convergence in comparison to a fixed value of 1.
+    max_section_sample_size = 100  # Note: Using 200 or 300 led to a very slow response requiring over a minute. Also see the note in its usage for convergence.
+    expected_report_length = "long"
+    num_successive_convergences_required = {"long": 1, "medium": 3, "short": 5}[expected_report_length]  # Observed counts of sections for a user query: 1 → 86; 2 → 19; 3 → 19; 5 → 11;
     rng = random.Random(0)
+    get_unique_sections: callable[[], set[str]] = lambda: {s for a in articles_and_sections for s in a["sections"]}
 
-    draft_to_final_section_candidate_counts: dict[str, dict[str, int]] = {}
-    cycle_num, iteration_num = 1, 0
-    num_unique_sections_by_cycle: list[int] = [num_unique_original_sections]
+    iteration_num = 0
+    prev_unique_sections: set[str] = get_unique_sections()
+    num_successive_convergences = 0
     while True:
-        unique_sections = {s for a in articles_and_sections for s in a["sections"]}
-        num_unique_sections = len(unique_sections)
-        num_unique_sections_finalized = sum((max(draft_to_final_section_candidate_counts.get(s, {}).values(), default=0) >= votes_needed_to_finalize_section) for s in unique_sections)
-        num_votes_cast = sum(sum(draft_to_final_section_candidate_counts.get(s, {}).values()) for s in unique_sections)
-        print(f"After iteration {iteration_num} in cycle {cycle_num}, with {num_votes_cast:,} active votes, the section name counts are: finalized={num_unique_sections_finalized} draft={num_unique_sections} original={num_unique_original_sections}")
-        if num_unique_sections == num_unique_sections_finalized:
-            num_unique_sections_by_cycle.append(num_unique_sections)
-            assert num_unique_sections_by_cycle[cycle_num] == num_unique_sections_by_cycle[-1] == num_unique_sections
-            if num_unique_sections_by_cycle[-1] == num_unique_sections_by_cycle[-2]:
-                print(f"Convergence reached after {iteration_num} iterations in cycle {cycle_num} for finalizing section names.")
-                break
-            cycle_num += 1
-            draft_to_final_section_candidate_counts.clear()
-        iteration_num += 1
-        # input("Press Enter to continue...")
+        unique_sections: set[str] = get_unique_sections()
+        num_unique_sections, num_prev_unique_sections = len(unique_sections), len(prev_unique_sections)
+        print(f"After iteration {iteration_num}, the section counts are: current={num_unique_sections} previous={num_prev_unique_sections} original={num_unique_original_sections}")
 
-        max_section_sample_size = {num_unique_sections >= 100: 100, num_unique_sections < 100: 50}[True]
-        # Note:
-        # * Using 200 or 300 led to a very slow response requiring over a minute.
-        # * Using 100 led to a convergence at just over 80 final sections.
-        # * Dynamically using 50 led to a convergence at just over 50 final sections.
+        if (iteration_num > 0) and (num_unique_sections <= max_section_sample_size) and (unique_sections == prev_unique_sections):
+            # Note: The condition `num_unique_sections <= max_section_sample_size` is added to ensure that convergence is over the entire population, not the sample.
+            num_successive_convergences += 1
+            print(f"Convergence {num_successive_convergences}/{num_successive_convergences_required} reached after {iteration_num} iterations for finalizing section names.")
+            if num_successive_convergences == num_successive_convergences_required:
+                break
+        elif num_successive_convergences > 0:
+            num_successive_convergences = 0
+        iteration_num += 1
+        prev_unique_sections = unique_sections
 
         sample_draft_sections = rng.sample(sorted(unique_sections), min(max_section_sample_size, num_unique_sections))  # Note: `sorted` is used to ensure deterministic sample selection.
         sample_draft_to_final_sections = _list_final_sections_for_sample(user_query, source_module, sample_draft_sections)
         for draft_section, final_section in sample_draft_to_final_sections.items():
-            draft_to_final_section_candidate_counts.setdefault(draft_section, {})
-            prev_max_final_section_candidate_count = max(draft_to_final_section_candidate_counts[draft_section].values(), default=0)
-            final_section_candidate_count = draft_to_final_section_candidate_counts[draft_section].get(final_section, 0) + 1
-            draft_to_final_section_candidate_counts[draft_section][final_section] = final_section_candidate_count
-            if (draft_section != final_section) and (final_section_candidate_count >= votes_needed_to_finalize_section) and (final_section_candidate_count >= prev_max_final_section_candidate_count):
-                # Note: `>=` is intentionally used when tie-breaking against prev_max_final_section_candidate_count. This is considering that the latest vote is considered to be over a more finalized sample, and is therefore preferred.
+            if draft_section != final_section:
                 for article in articles_and_sections:
                     article_sections = article["sections"]
                     assert article_sections
@@ -201,6 +200,6 @@ def list_final_sections(user_query: str, source_module: ModuleType, articles_and
                             article_sections.append(final_section)
                         assert draft_section not in article["sections"]
                         assert final_section in article["sections"]
-                print(f"In iteration {iteration_num} in cycle {cycle_num}, renamed draft section {draft_section!r} to final section {final_section!r}.")
+                print(f"In iteration {iteration_num}, renamed draft section {draft_section!r} to final section {final_section!r}.")
 
     return articles_and_sections
