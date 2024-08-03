9876f8b09bd2a865bd3df98987b1680d2a45f1e1
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-08-03 05:05:04 +0000

    Print articles by final section

--- src/newsqa/newsqa.py
+++ src/newsqa/newsqa.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Optional
 
-from newsqa.types import AnalyzedArticle, SearchResult
+from newsqa.types import AnalyzedArticle, SearchArticle, SearchResult
 from newsqa.util.input import get_confirmation
 from newsqa.util.openai_ import ensure_openai_key, MODELS
 from newsqa.workflow.user.query import ensure_query_is_valid
@@ -48,11 +48,14 @@ def generate_response(source: str, query: str, output_path: Optional[Path] = Non
     if confirm:
         get_confirmation("draft sections")
     articles_and_draft_sections: list[AnalyzedArticle] = list_draft_sections(user_query=query, source_module=source_module, search_results=search_results)
-    print("DRAFT SECTIONS:\n" + "\n".join(f'#{num}: {a["article"]["title"]}\n\t{"\n\t".join(a["sections"])}' for num, a in enumerate(articles_and_draft_sections, start=1)))
+    print("DRAFT SECTIONS BY ARTICLE:\n" + "\n".join(f'#{num}: {a["article"]["title"]} ({len(a["sections"])} sections)\n\t{"\n\t".join(a["sections"])}' for num, a in enumerate(articles_and_draft_sections, start=1)))
 
     if confirm:
         get_confirmation("final sections")
     articles_and_final_sections: list[AnalyzedArticle] = list_final_sections(user_query=query, source_module=source_module, articles_and_draft_sections=articles_and_draft_sections)
-    print("FINAL SECTIONS BY ARTICLE:\n" + "\n".join(f'#{num}: {a["article"]["title"]}\n\t{"\n\t".join(a["sections"])}' for num, a in enumerate(articles_and_final_sections, start=1)))
+    print("FINAL SECTIONS BY ARTICLE:\n" + "\n".join(f'#{article_num}: {a["article"]["title"]} ({len(a["sections"])} sections)\n\t{"\n\t".join(a["sections"])}' for article_num, a in enumerate(articles_and_final_sections, start=1)))
     final_sections_sorted: list[str] = sorted({section for a in articles_and_final_sections for section in a["sections"]})
-    print(f"FINAL SECTIONS SORTED ({len(final_sections_sorted)}):\n" + "\n".join(f"{num}: {section}" for num, section in enumerate(final_sections_sorted, start=1)))
+    articles_by_section: dict[str, list[SearchArticle]] = {section: [a["article"] for a in articles_and_final_sections if section in a["sections"]] for section in final_sections_sorted}
+    print("ARTICLES BY FINAL SECTION:\n" + "\n".join(f"{section_num}. {section} ({len(articles_by_section[section])} articles)\n\t" + "\n\t".join(f'{article_num}: {a["title"]}' for article_num, a in enumerate(articles_by_section[section], start=1)) for section_num, section in enumerate(final_sections_sorted, start=1)))
+    print(f"FINAL SECTIONS SORTED ({len(final_sections_sorted)}):\n" + "\n".join(f"{num}: {section} ({len(articles_by_section[section])} articles)" for num, section in enumerate(final_sections_sorted, start=1)))
+    del final_sections_sorted, articles_by_section
