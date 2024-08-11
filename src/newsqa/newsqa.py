54f1bf1f11b339c112c55986c08f21342dd1b05f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-08-11 02:24:50 +0000

    Update section header in print message

--- src/newsqa/newsqa.py
+++ src/newsqa/newsqa.py
@@ -58,8 +58,7 @@ def generate_response(source: str, query: str, output_path: Optional[Path] = Non
     final_sections_ordered: list[str] = sorted({section for a in articles_and_final_sections for section in a["sections"]}, key=lambda section: len([a for a in articles_and_final_sections if section in a["sections"]]), reverse=True)
     articles_by_section: dict[str, list[SearchArticle]] = {section: [a["article"] for a in articles_and_final_sections if section in a["sections"]] for section in final_sections_ordered}
     print("ARTICLES BY FINAL SECTION:\n" + "\n".join(f"{section_num}. {section} ({len(articles_by_section[section])} articles)\n\t" + "\n\t".join(f'{article_num}: {a["title"]}' for article_num, a in enumerate(articles_by_section[section], start=1)) for section_num, section in enumerate(final_sections_ordered, start=1)))
-    print(f"FINAL SECTIONS ORDERED ({len(final_sections_ordered)}):\n" + "\n".join(f"{num}: {section} ({len(articles_by_section[section])} articles)" for num, section in enumerate(final_sections_ordered, start=1)))
-    del final_sections_ordered, articles_by_section
+    print(f"FINAL SECTIONS ORDERED BY ARTICLE COUNT ({len(final_sections_ordered)}):\n" + "\n".join(f"{num}: {section} ({len(articles_by_section[section])} articles)" for num, section in enumerate(final_sections_ordered, start=1)))
 
     # if confirm:
     #     get_confirmation("ordering final sections")
