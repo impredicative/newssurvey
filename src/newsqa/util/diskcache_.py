689faf9c4512c6c2add744bc37b83f615a0c36e3
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-08-20 18:38:39 +0000

    Update diskcache timeout to 10 seconds

--- src/newsqa/util/diskcache_.py
+++ src/newsqa/util/diskcache_.py
@@ -21,4 +21,4 @@ def get_diskcache(file_path: str, *, size_gib: int = 1) -> diskcache.FanoutCache
     path = path.with_suffix("")  # Ex: sources/medicalxpress/article
     path = DISKCACHE_ROOT_PATH / path  # Ex: /home/user/projects/newsqa/src/newsqa/.diskcache/sources/medicalxpress/article
     print(f"Using diskcache path: {path}")
-    return diskcache.FanoutCache(directory=str(path), timeout=1, size_limit=size_gib * GiB)
+    return diskcache.FanoutCache(directory=str(path), timeout=10, size_limit=size_gib * GiB)
