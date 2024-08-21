fdcfeb66e50c988ca60d9256fae15a43a5a83e0e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-08-21 02:01:48 +0000

    Add wip list_final_sections_using_all module

---
+++ src/newsqa/workflow/llm/list_final_sections_using_all.py
@@ -0,0 +1,108 @@
+import contextlib
+import copy
+import io
+import re
+from types import ModuleType
+
+from newsqa.config import PROMPTS, NUM_SECTIONS_MIN, NUM_SECTIONS_MAX
+from newsqa.exceptions import LanguageModelOutputStructureError, SourceInsufficiencyError
+from newsqa.types import AnalyzedArticle, SearchArticle, SearchResult
+from newsqa.util.openai_ import get_content
+from newsqa.util.scipy_ import sort_by_distance
+from newsqa.util.sys_ import print_error, print_warning
+
+_SECTION_PATTERN = re.compile(r"(?P<num>\d+)\. (?P<section>.+?)")
+
+def _are_sections_valid(numbered_sections: list[str]) -> bool:
+    """Return true if the section names are valid, otherwise false.
+    
+    A validation error is printed if a section name is invalid.
+
+    Example of valid section: "3. Epidemiology of daytime drowsiness"
+    """
+    if not numbered_sections:
+        print_error("No sections found.")
+        return False
+
+    seen = set()
+    for num, numbered_section in enumerate(numbered_sections, start=1):
+        if not isinstance(numbered_section, str):
+            print_error(f"Section {num} is not a string.")
+            return False
+
+        match = _SECTION_PATTERN.fullmatch(numbered_section)
+        if not match:
+            print_error(f"Section line #{num} does not match expected pattern. The section string is: {numbered_section!r}")
+            return False
+        
+        section_num = int(match["num"])
+        if section_num != num:
+            print_error(f"Section number #{num} is not sequential. The section string is: {numbered_section!r}")
+            return False
+        
+        section = match["section"]
+        section_casefold = section.casefold()
+        if section_casefold in seen:
+            print_error(f"Section name #{num} is a duplicate. The section string is: {numbered_section!r}")
+            return False
+        seen.add(section_casefold)
+
+    return True
+
+
+def _list_final_sections(user_query: str, source_module: ModuleType, draft_sections: list[str], *, max_sections: int, max_attempts: int = 3) -> list[str]:
+    assert user_query
+    assert draft_sections
+
+    # TODO: Truncate sections if prompt is too long. Refer to:
+    #       https://platform.openai.com/docs/advanced-usage/managing-tokens 
+    #       https://github.com/openai/tiktoken/issues/305
+    #       https://github.com/openai/tiktoken/issues/98
+    #       https://cookbook.openai.com/examples/how_to_count_tokens_with_tiktoken
+
+    prompt_data = {"user_query": user_query, "source_site_name": source_module.SOURCE_SITE_NAME, "source_type": source_module.SOURCE_TYPE}
+    numbered_draft_sections = [f"{i}. {s}" for i, s in enumerate(draft_sections, start=1)]
+    numbered_draft_sections_str = "\n".join(numbered_draft_sections)
+    prompt_data["task"] = PROMPTS["4. list_final_sections_using_all-initial"].format(**prompt_data, draft_sections=numbered_draft_sections_str, max_sections=max_sections)
+    prompt = PROMPTS["0. common"].format(**prompt_data)
+
+    for num_attempt in range(1, max_attempts + 1):
+        response = get_content(prompt, model_size="large", log=True or (num_attempt == max_attempts), read_cache=(num_attempt == 1))
+        
+        numbered_response_sections = [line.strip() for line in response.splitlines()]
+        numbered_response_sections = [line for line in numbered_response_sections if line]
+    
+        error = io.StringIO()
+        with contextlib.redirect_stderr(error):
+            response_is_valid = _are_sections_valid(numbered_response_sections)
+        if not response_is_valid:
+            error = error.getvalue().rstrip().removeprefix("Error: ")
+            if num_attempt == max_attempts:
+                raise LanguageModelOutputStructureError(error)
+            else:
+                print_warning(f"Fault in attempt {num_attempt} of {max_attempts} while getting final section names: {error}")
+                continue
+
+        break
+
+    numbered_response_matches = [_SECTION_PATTERN.fullmatch(line) for line in numbered_response_sections]
+    final_sections = [match["section"] for match in numbered_response_matches]
+    return final_sections
+
+
+def list_final_sections(user_query: str, source_module: ModuleType, *, articles_and_draft_sections: list[AnalyzedArticle], max_sections: int) -> list[AnalyzedArticle]:
+    """Return a list of dictionaries containing the search article and respective final section names.
+
+    The internal functions raise `LanguageModelOutputError` if the model output has an error.
+    Specifically, its subclass `LanguageModelOutputStructureError` is raised by it if the output is structurally invalid.
+    """
+    assert NUM_SECTIONS_MIN <= max_sections <= NUM_SECTIONS_MAX, (max_sections, NUM_SECTIONS_MIN, NUM_SECTIONS_MAX)
+
+    articles_and_sections = copy.deepcopy(articles_and_draft_sections)
+    del articles_and_draft_sections  # Note: This prevents accidental modification of draft sections.
+
+    draft_sections = list({s for a in articles_and_sections for s in a["sections"]})
+    draft_sections = sort_by_distance(user_query, draft_sections, model_size="large", distance="cosine")
+    final_sections = _list_final_sections(user_query, source_module, draft_sections, max_sections=max_sections)
+
+    exit()
