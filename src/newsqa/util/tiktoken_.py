7ff04b1d50233c54e14c2bbc05240430ff86b06d
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-08-26 23:03:23 +0000

    Rename and update fit_input_items_to_token_limit function to use formatter kwarg

--- src/newsqa/util/tiktoken_.py
+++ src/newsqa/util/tiktoken_.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Callable, Optional
 
 import tiktoken
 
@@ -41,63 +41,63 @@ def is_input_token_usage_allowable(text: str, *, model: str, usage: Optional[dic
     return usage["num_tokens"] <= usage["max_tokens"]
 
 
-def fit_input_parts_to_token_limit(parts: list[str], *, model: str, sep: str = "\n", approach: str = "binary") -> str:
-    """Return a text that fits the input token limit for the given parts and model.
+def fit_input_items_to_token_limit(items: list, *, model: str, formatter: Callable[[list], str] = lambda seq: "\n".join(seq), approach: str = "binary") -> str:
+    """Return a text that fits the input token limit for the given items and model.
 
-    The parts are joined by the given separator.
+    The items are formatted to a string using the given formatter function.
     """
     # Tests:
-    # _=fit_input_parts_to_token_limit([string.printable]*10_000, model="gpt-4o-2024-08-06", approach='binary') -> Using 3,487/10,000 parts of text for model gpt-4o-2024-08-06 and encoding o200k_base, with 111,583/111,606 tokens.
-    # _=fit_input_parts_to_token_limit(''.join(random.Random(0).choices(string.printable, k=1_000_000)).split('\n'), model="gpt-4o-2024-08-06", approach='binary') -> Using 1,480/9,929 parts of text for model gpt-4o-2024-08-06 and encoding o200k_base, with 111,410/111,606 tokens.
+    # _=fit_input_items_to_token_limit([string.printable]*10_000, model="gpt-4o-2024-08-06", approach='binary') -> Using 3,487/10,000 items of text for model gpt-4o-2024-08-06 and encoding o200k_base, with 111,583/111,606 tokens.
+    # _=fit_input_items_to_token_limit(''.join(random.Random(0).choices(string.printable, k=1_000_000)).split('\n'), model="gpt-4o-2024-08-06", approach='binary') -> Using 1,480/9,929 items of text for model gpt-4o-2024-08-06 and encoding o200k_base, with 111,410/111,606 tokens.
     encoding = get_encoding(model).name
-    text = sep.join(parts)
+    text = formatter(items)
     usage = calc_input_token_usage(text, model=model)
-    num_parts = len(parts)
+    num_items = len(items)
     if is_input_token_usage_allowable(text, model=model, usage=usage):
-        print(f"Using all {num_parts:,} parts of text for model {model} and encoding {encoding}, with {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
+        print(f"Using all {num_items:,} items of text for model {model} and encoding {encoding}, with {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
         return text
 
     iteration = 0
     match approach:
         case "binary":
             # Binary search
-            lo, hi = 1, num_parts
+            lo, hi = 1, num_items
             while lo < hi:
                 iteration += 1
                 mid = (lo + hi + 1) // 2
-                mid_text = sep.join(parts[:mid])
+                mid_text = formatter(items[:mid])
                 usage = calc_input_token_usage(mid_text, model=model)
-                num_parts_used = mid
+                num_items_used = mid
                 if is_input_token_usage_allowable(mid_text, model=model, usage=usage):
                     lo = mid
                 else:
                     hi = mid - 1
-                print(f"Tried {num_parts_used:,}/{num_parts:,} parts of text for model {model} in iteration {iteration:,} using {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
-            num_parts_used = lo
+                print(f"Tried {num_items_used:,}/{num_items:,} items of text for model {model} in iteration {iteration:,} using {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
+            num_items_used = lo
         case "rate":
             # Rate-based search
-            num_parts_used = num_parts
-            parts_to_excess_tokens: dict[int, int] = {}
+            num_items_used = num_items
+            items_to_excess_tokens: dict[int, int] = {}
             while True:
-                if (num_parts_used in parts_to_excess_tokens) and (parts_to_excess_tokens[num_parts_used] <= 0):
-                    num_parts_used = max({p: e for p, e in parts_to_excess_tokens.items() if e < 0}, key=parts_to_excess_tokens.__getitem__)
-                    candidate_num_parts_used = num_parts_used + 1
-                    if (candidate_num_parts_used in parts_to_excess_tokens) and (parts_to_excess_tokens[candidate_num_parts_used] > 0):
+                if (num_items_used in items_to_excess_tokens) and (items_to_excess_tokens[num_items_used] <= 0):
+                    num_items_used = max({p: e for p, e in items_to_excess_tokens.items() if e < 0}, key=items_to_excess_tokens.__getitem__)
+                    candidate_num_items_used = num_items_used + 1
+                    if (candidate_num_items_used in items_to_excess_tokens) and (items_to_excess_tokens[candidate_num_items_used] > 0):
                         break
-                    num_parts_used = candidate_num_parts_used
+                    num_items_used = candidate_num_items_used
                 iteration += 1
-                usage = calc_input_token_usage(sep.join(parts[:num_parts_used]), model=model)
-                parts_to_excess_tokens[num_parts_used] = usage["num_tokens"] - usage["max_tokens"]
+                usage = calc_input_token_usage(formatter(items[:num_items_used]), model=model)
+                items_to_excess_tokens[num_items_used] = usage["num_tokens"] - usage["max_tokens"]
                 rate = usage["num_tokens"] / usage["max_tokens"]
-                print(f"Tried {num_parts_used:,}/{num_parts:,} parts of text for model {model} in iteration {iteration:,} using {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
-                num_parts_used = num_parts_used / rate
-                num_parts_used = int(num_parts_used)  # Note: Always using `round` or `math.ceil` instead of `int` was observed to lead to an infinite loop.
-                num_parts_used = min(num_parts, num_parts_used)
+                print(f"Tried {num_items_used:,}/{num_items:,} items of text for model {model} in iteration {iteration:,} using {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
+                num_items_used = num_items_used / rate
+                num_items_used = int(num_items_used)  # Note: Always using `round` or `math.ceil` instead of `int` was observed to lead to an infinite loop.
+                num_items_used = min(num_items, num_items_used)
         case _:
             raise ValueError(f"Unsupported approach {approach!r}.")
 
     encoding = get_encoding(model).name
-    text_used = sep.join(parts[:num_parts_used])
+    text_used = formatter(items[:num_items_used])
     usage = calc_input_token_usage(text_used, model=model)
-    print(f"Using {num_parts_used:,}/{num_parts:,} parts of text for model {model} and encoding {encoding}, with {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
+    print(f"Using {num_items_used:,}/{num_items:,} items of text for model {model} and encoding {encoding}, with {usage['num_tokens']:,}/{usage['max_tokens']:,} tokens.")
     return text_used
