4da76f59c84f8592179f973395df413256e4494c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-07 20:55:38 +0000

    Update diskcache_ util module comments to use workspaces paths

--- src/newssurvey/util/diskcache_.py
+++ src/newssurvey/util/diskcache_.py
@@ -15,10 +15,10 @@ def get_diskcache(file_path: str, *, size_gib: int = 1) -> diskcache.FanoutCache
 
     `file_path` should typically be `__file__`.
     """
-    path = Path(file_path)  # Ex: /home/user/projects/newssurvey/src/newssurvey/sources/medicalxpress/article.py
+    path = Path(file_path)  # Ex: /workspaces/newssurvey/src/newssurvey/sources/medicalxpress/article.py
     assert path.is_relative_to(PACKAGE_PATH), path
     path = path.relative_to(PACKAGE_PATH)  # Ex: sources/medicalxpress/article.py
     path = path.with_suffix("")  # Ex: sources/medicalxpress/article
-    path = DISKCACHE_ROOT_PATH / path  # Ex: /home/user/projects/newssurvey/src/newssurvey/.diskcache/sources/medicalxpress/article
+    path = DISKCACHE_ROOT_PATH / path  # Ex: /workspaces/newssurvey/src/newssurvey/.diskcache/sources/medicalxpress/article
     print(f"Using diskcache path: {path}")
     return diskcache.FanoutCache(directory=str(path), timeout=10, size_limit=size_gib * GiB)
