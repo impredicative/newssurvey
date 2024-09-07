d20e07a128a9563497e2c86a2d9090e98baa2186
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-07 03:28:32 +0000

    Update `main` function to write response to output file

--- src/newsqa/__main__.py
+++ src/newsqa/__main__.py
@@ -15,14 +15,14 @@ from newsqa.workflow.user.source import get_source, get_source_module, ensure_so
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"], "max_content_width": 120})
 @click.option("--source", "-s", default=None, help="Name of supported news source. If not given, the user is prompted for it.")
-@click.option("--query", "-q", default=None, help="Question or concern answerable by the news source. If a path to a file, the file text is read. If not given, the user is prompted for it.")
+@click.option("--query", "-q", default=None, help="Question or concern answerable by the news source. If a path to a file, the file text is read as text. If not given, the user is prompted for it.")
 @click.option("--max-sections", "-m", default=NUM_SECTIONS_DEFAULT, type=click.IntRange(NUM_SECTIONS_MIN, NUM_SECTIONS_MAX), help=f"Maximum number of sections to include in the response, between {NUM_SECTIONS_MIN} and {NUM_SECTIONS_MAX}. Its recommended value, also the default, is {NUM_SECTIONS_DEFAULT}.")
-@click.option("--output-path", "-o", default=None, type=Path, help="Output file path. If given, the response is also written to this text file except if there is an error.")
+@click.option("--output-path", "-o", required=True, type=Path, help="Output text file path with extension txt. The response is written to this file except if there is an error.")
 @click.option("--confirm/--no-confirm", "-c/-nc", default=True, help="Confirm as the workflow progresses. If `--confirm`, a confirmation is interactively sought as each step of the workflow progresses, and this is the default. If `--no-confirm`, the workflow progresses without any confirmation.")
-def main(source: Optional[str], query: Optional[str], max_sections: int, output_path: Optional[Path], confirm: bool) -> None:
-    """Generate, print, and optionally write a response to a question or concern using a supported news source.
+def main(source: Optional[str], query: Optional[str], max_sections: int, output_path: Path, confirm: bool) -> None:
+    """Generate and write a response to a question or concern using a supported news source.
 
-    The progress and response both are printed to stdout.
+    The progress is printed to stdout.
 
     A nonzero exitcode exists if there is an error.
     """
@@ -44,12 +44,16 @@ def main(source: Optional[str], query: Optional[str], max_sections: int, output_
         assert isinstance(max_sections, int), (max_sections, type(max_sections))
         assert NUM_SECTIONS_MIN <= max_sections <= NUM_SECTIONS_MAX, (max_sections, NUM_SECTIONS_MIN, NUM_SECTIONS_MAX)
 
-        if output_path:
-            assert isinstance(output_path, Path), (output_path, type(output_path))
+        assert isinstance(output_path, Path), (output_path, type(output_path))
+        assert (not output_path.is_dir()), output_path
+        assert (output_path.suffix == ".txt"), (output_path, output_path.suffix)
+
         assert isinstance(confirm, bool), (confirm, type(confirm))
 
         response = generate_response(source=source, query=query, max_sections=max_sections, output_path=output_path, confirm=confirm)
-        print(response)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        output_path.write_text(response)
+        print(f"Wrote response to {output_path}.")
     except newsqa.exceptions.Error as exc:
         print_error(str(exc))
         query_sep = "\n" if (isinstance(query, str) and (len(query.splitlines()) > 1)) else " "
