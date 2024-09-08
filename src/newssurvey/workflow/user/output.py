c5f5bef7b91fdd8f01f1422d1a63457d8777a074
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-08 23:58:58 +0000

    Update format_text_output and format_markdown_output

--- src/newssurvey/workflow/user/output.py
+++ src/newssurvey/workflow/user/output.py
@@ -11,16 +11,16 @@ def _get_date_string() -> str:
 
 
 def format_text_output(title: str, sections: list[SectionGen2], citations: list[CitationGen2]) -> str:
-    """Return the formatted text output for the given sections and citations."""
+    """Return the text output for the given sections and citations."""
     sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(r"[\1]", section["text"])) for section in sections]  # Use standard brackets for citations.
 
-    text = f"{title}\n\n" + f"{_get_date_string()}\n\n" + "Sections:\n" + "\n".join([f"{num}: {section['title']}" for num, section in enumerate(sections, start=1)]) + "\n\n" + "\n\n".join(f'Section {num}. {s["title"]}:\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + "References:\n\n" + "\n\n".join([f"{c['number']}: {c['title']}\n{c['link']}" for c in citations])
+    text = f"{title}\n\n" + f"{_get_date_string()}\n\n" + "Sections:\n" + "\n".join([f"{num}: {section['title']}" for num, section in enumerate(sections, start=1)]) + "\n\n" + "\n\n".join(f'Section {num}: {s["title"]}:\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + "References:\n\n" + "\n\n".join([f"{c['number']}: {c['title']}\n{c['link']}" for c in citations])
     return text
 
 
 def format_markdown_output(title: str, sections: list[SectionGen2], citations: list[CitationGen2]) -> str:
-    """Return the formatted Markdown output for the given sections and citations."""
+    """Return the GitHub Flavored markdown output for the given sections and citations."""
     sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(r"[\1]", section["text"])) for section in sections]  # Use standard brackets for citations.
 
-    text = f"# {title}\n\n" + f"{_get_date_string()}\n\n" + "## Sections:\n" + "\n".join([f"- {section['title']}" for section in sections]) + "\n\n" + "\n\n".join(f'## Section {num}. {s["title"]}:\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + "## References:\n\n" + "\n\n".join([f"- {c['title']}\n  - {c['link']}" for c in citations])
+    text = f"# {title}\n\n" + f"{_get_date_string()}\n\n" + "## Sections:\n" + "\n".join([f"{num}. {section['title']}" for num, section in enumerate(sections, start=1)]) + "\n\n" + "\n\n".join(f'## Section {num}: {s["title"]}\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + "## References:\n\n" + "\n\n".join([f"{c['number']}. [{c['title']}]({c['link']})" for c in citations])
     return text
