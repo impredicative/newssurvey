3bf64490483488e49e795ac7486def78ed3509bb
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-09 02:46:07 +0000

    Refactor format_markdown_output function to include table of contents and references

--- src/newssurvey/workflow/user/output.py
+++ src/newssurvey/workflow/user/output.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from newssurvey.config import CITATION_GROUP_PATTERN
 from newssurvey.types import SectionGen2, CitationGen2
@@ -12,7 +13,7 @@ def _get_date_string() -> str:
 
 def format_text_output(title: str, sections: list[SectionGen2], citations: list[CitationGen2]) -> str:
     """Return the text output for the given sections and citations."""
-    sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(r"[\1]", section["text"])) for section in sections]  # Use standard brackets for citations.
+    sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(r"[\1]", section["text"])) for section in sections]  # Uses standard brackets for citations.
 
     text = f"{title}\n\n" + f"{_get_date_string()}\n\n" + "Sections:\n" + "\n".join([f"{num}: {section['title']}" for num, section in enumerate(sections, start=1)]) + "\n\n" + "\n\n".join(f'Section {num}: {s["title"]}:\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + "References:\n\n" + "\n\n".join([f"{c['number']}: {c['title']}\n{c['link']}" for c in citations])
     return text
@@ -21,10 +22,13 @@ def format_text_output(title: str, sections: list[SectionGen2], citations: list[
 def format_markdown_output(title: str, sections: list[SectionGen2], citations: list[CitationGen2]) -> str:
     """Return the GitHub Flavored markdown output for the given sections and citations."""
     contents = [f"{num}. [{section['title']}](#section-{num})" for num, section in enumerate(sections, start=1)]
-    references_contents_num = len(contents) + 1
-    contents.append(f"{references_contents_num}. [References](#references)")
+    contents.append(f"{len(contents) + 1}. [References](#references)")
 
-    sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(r"[\1]", section["text"])) for section in sections]  # Use standard brackets for citations.
+    def repl(match: re.Match) -> str:
+        """Replace the plain citation numbers in the citation group with linked citation numbers."""
+        return "[" + ",".join(f'[{citation_num}](#citation-{citation_num})' for citation_num in match.group(1).split(",")) + "]"
 
-    text = f"# {title}\n\n" + f"{_get_date_string()}\n\n" + "## Contents:\n" + "\n".join(contents) + "\n\n" + "\n\n".join(f'## <a id="section-{num}"></a>{num}. {s["title"]}\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + f'## <a id="references"></a>{references_contents_num}. References\n\n' + "\n\n".join([f"{c['number']}. [{c['title']}]({c['link']})" for c in citations])
+    sections = [SectionGen2(title=section["title"], text=CITATION_GROUP_PATTERN.sub(repl, section["text"])) for section in sections]
+
+    text = f"# {title}\n\n" + f"{_get_date_string()}\n\n" + "## Contents\n" + "\n".join(contents) + "\n\n" + "\n\n".join(f'## <a id="section-{num}"></a>{num}. {s["title"]}\n\n{s["text"]}' for num, s in enumerate(sections, start=1)) + "\n\n" + '## <a id="references"></a>References\n\n' + "\n\n".join([f'<a id="citation-{c["number"]}"></a>{c["number"]}. [{c["title"]}]({c["link"]})' for c in citations])
     return text
