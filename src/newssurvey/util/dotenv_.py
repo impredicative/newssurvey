e56b17656cf757717c69281bbe2d8991f88498a0
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-11 07:50:33 +0000

    Refactor dotenv loading to include current directory

--- src/newssurvey/util/dotenv_.py
+++ src/newssurvey/util/dotenv_.py
@@ -4,4 +4,4 @@ import dotenv
 def load_dotenv():
     """Load the .env file normally and also from the current directory."""
     dotenv.load_dotenv()
-    dotenv.find_dotenv(usecwd=True)
+    dotenv.load_dotenv(dotenv.find_dotenv(usecwd=True))
