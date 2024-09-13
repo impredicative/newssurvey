43f6ad37cf2898f36089e7958c15c5e7a1e8b66d
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-13 07:32:51 +0000

    Update calc_input_token_usage function to use num_output_tokens instead of max_output_tokens

--- src/newssurvey/util/tiktoken_.py
+++ src/newssurvey/util/tiktoken_.py
@@ -20,11 +20,12 @@ def count_tokens(text: str, *, model: str) -> int:
     return len(encoded)
 
 
-def calc_input_token_usage(text: str, *, model: str, max_output_tokens: Optional[int] = None) -> dict[str, int]:
+def calc_input_token_usage(text: str, *, model: str, num_output_tokens: Optional[int] = None) -> dict[str, int]:
     """Return the input token usage for the given text and model.
 
-    If `max_output_tokens` is not given, it is set to the maximum output tokens for the given model.
-    If `max_output_tokens` is given, it must be less than or equal to the maximum output tokens for the given model.
+    `num_output_tokens` is the allocated number of tokens for the output text.
+    If given, it must be less than or equal to the maximum output tokens for the given model.
+    If not given, it is set to the maximum output tokens for the given model.
 
     The returned dictionary has the following keys:
     * `used_tokens`: Number of tokens used by input.
@@ -32,12 +33,14 @@ def calc_input_token_usage(text: str, *, model: str, max_output_tokens: Optional
     """
     used_tokens = count_tokens(text, model=model)
 
-    if max_output_tokens is None:
-        assert max_output_tokens <= MAX_OUTPUT_TOKENS[model], (max_output_tokens, MAX_OUTPUT_TOKENS[model])
-        max_output_tokens = MAX_OUTPUT_TOKENS[model]
-    assert MAX_INPUT_TOKENS[model] >= max_output_tokens
+    if num_output_tokens is None:
+        num_output_tokens = MAX_OUTPUT_TOKENS[model]
+    else:
+        assert num_output_tokens <= MAX_OUTPUT_TOKENS[model], (num_output_tokens, MAX_OUTPUT_TOKENS[model])
+    
+    assert MAX_INPUT_TOKENS[model] >= num_output_tokens
 
-    usable_tokens = max(0, MAX_INPUT_TOKENS[model] - max_output_tokens - (HEADER_TOKENS_PER_MESSAGE * 2) - FOOTER_TOKENS)
+    usable_tokens = max(0, MAX_INPUT_TOKENS[model] - num_output_tokens - (HEADER_TOKENS_PER_MESSAGE * 2) - FOOTER_TOKENS)
     return {"used_tokens": used_tokens, "usable_tokens": usable_tokens}
 
 
