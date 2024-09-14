3da07909d9f3f0f17686eef83c3df6ffb4aaf007
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-14 23:30:05 +0000

    Add difflib utility module for string difference comparison

---
+++ src/newssurvey/util/difflib_.py
@@ -0,0 +1,5 @@
+import difflib
+
+def ndiffstr(a: list[str], b: list[str]) -> str:
+    """Return the difference between two lists of strings as a string."""
+    return '\n'.join(difflib.ndiff(a, b))
