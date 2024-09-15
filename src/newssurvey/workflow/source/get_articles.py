da7c99e4a525e029898d699e6ba2f371978057ff
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-15 19:08:06 +0000

    Refactor get_articles function to increase max_workers to 4 for improved performance

--- src/newssurvey/workflow/source/get_articles.py
+++ src/newssurvey/workflow/source/get_articles.py
@@ -15,6 +15,6 @@ def _get_article(source_module: ModuleType, search_result: SearchResult) -> Sear
 def get_articles(source_module: ModuleType, search_results: list[SearchResult]) -> list[SearchArticle]:
     """Return a list of articles for the given search results."""
     fetch_article = lambda search_result: _get_article(source_module, search_result)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         articles = list(executor.map(fetch_article, search_results))
     return articles
