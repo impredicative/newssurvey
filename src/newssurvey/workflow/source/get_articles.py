1e132e547a9c2b8d79da8e9f1df1ad0aafd594b2
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-09-17 03:02:41 +0000

    Update get_articles function to print number of articles received

--- src/newssurvey/workflow/source/get_articles.py
+++ src/newssurvey/workflow/source/get_articles.py
@@ -1,20 +1,24 @@
 import concurrent.futures
+import itertools
 from types import ModuleType
+
 from newssurvey.types import SearchArticle, SearchResult
-
-
-def _get_article(source_module: ModuleType, search_result: SearchResult) -> SearchArticle:
-    """Return the article for the given search result."""
-    article_text = source_module.get_article_text(search_result["link"])
-    article = SearchArticle(**search_result, text=article_text)
-    assert article_text.startswith(search_result["title"]), article  # If this fails, fix the parsing to ensure it is true.
-    print(f'Received article text for {article["title"]!r}.')
-    return article
 
 
 def get_articles(source_module: ModuleType, search_results: list[SearchResult]) -> list[SearchArticle]:
     """Return a list of articles for the given search results."""
-    fetch_article = lambda search_result: _get_article(source_module, search_result)
+    num_articles = len(search_results)
+    counter = itertools.count(1)
+
+    def _get_article(search_result: SearchResult) -> SearchArticle:
+        """Return the article for the given search result."""
+        article_text = source_module.get_article_text(search_result["link"])
+        article = SearchArticle(**search_result, text=article_text)
+        assert article_text.startswith(search_result["title"]), article  # If this fails, fix the parsing to ensure it is true.
+        count = next(counter)
+        print(f'Received text of article {count}/{num_articles} having title: {article["title"]}')
+        return article
+    
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        articles = list(executor.map(fetch_article, search_results))
+        articles = list(executor.map(_get_article, search_results))
     return articles
