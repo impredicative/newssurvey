f397a3a25edc6be9a41e7a7a824596912155676e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-10-22 20:13:25 +0000

    Increase sleep period between medicalxpress and physorg requests to 4 seconds, previously 3

--- src/newssurvey/sources/medicalxpress/_common.py
+++ src/newssurvey/sources/medicalxpress/_common.py
@@ -1,9 +1,9 @@
 from newssurvey.config import REQUEST_HEADERS
 from newssurvey.util.threading_ import CooldownLock
 
-_REQUEST_COOLDOWN = 3  # Applicable to default Mozilla Firefox user agent.
+_REQUEST_COOLDOWN = 4  # Applicable to default Mozilla Firefox user agent. Values ≤3 led to HTTP 422 and 429 errors, also requiring subsequent "press-and-hold" "security verification" via a web browser.
 REQUEST_HEADERS = REQUEST_HEADERS.copy()
 SOURCE_SITE_NAME = "MedicalXpress"
 SOURCE_TYPE = "medical"
 
-request_cooldown_lock = CooldownLock(cooldown=_REQUEST_COOLDOWN, name=SOURCE_SITE_NAME)  # Note: 2s or lower values of cooldown led to HTTP 429 error.
+request_cooldown_lock = CooldownLock(cooldown=_REQUEST_COOLDOWN, name=SOURCE_SITE_NAME)
--- src/newssurvey/sources/physorg/_common.py
+++ src/newssurvey/sources/physorg/_common.py
@@ -1,9 +1,9 @@
 from newssurvey.config import REQUEST_HEADERS
 from newssurvey.util.threading_ import CooldownLock
 
-_REQUEST_COOLDOWN = 3  # Applicable to default Mozilla Firefox user agent.
+_REQUEST_COOLDOWN = 4  # Applicable to default Mozilla Firefox user agent. Values ≤2 led to HTTP 429 errors.
 REQUEST_HEADERS = REQUEST_HEADERS.copy()
 SOURCE_SITE_NAME = "PhysOrg"
 SOURCE_TYPE = "science"
 
-request_cooldown_lock = CooldownLock(cooldown=_REQUEST_COOLDOWN, name=SOURCE_SITE_NAME)  # Note: 2s or lower values of cooldown are expected to lead to an HTTP 429 error.
+request_cooldown_lock = CooldownLock(cooldown=_REQUEST_COOLDOWN, name=SOURCE_SITE_NAME)
