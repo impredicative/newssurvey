8c720bfaeaa390f6ed8e9e8588f24f70dfe78142
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-10-22 23:39:29 +0000

    Update user agent and cooldown period for MedicalXpress and PhysOrg sources to AhrefsBot and 3

--- src/newssurvey/sources/medicalxpress/_common.py
+++ src/newssurvey/sources/medicalxpress/_common.py
@@ -1,8 +1,9 @@
-from newssurvey.config import REQUEST_HEADERS
+# from newssurvey.config import REQUEST_HEADERS
 from newssurvey.util.threading_ import CooldownLock
 
-_REQUEST_COOLDOWN = 5  # Applicable to default Mozilla Firefox user agent. Values ≤4 led to HTTP 422 and 429 errors, also requiring subsequent "press-and-hold" "security verification" via a web browser.
-REQUEST_HEADERS = REQUEST_HEADERS.copy()
+# _REQUEST_COOLDOWN = 5  # Applicable to default Mozilla Firefox user agent. Values ≤4 led to HTTP 422 and 429 errors, also requiring subsequent "press-and-hold" "security verification" via a web browser.
+_REQUEST_COOLDOWN = 3  # Exceeds observed value of 2 for AhrefsBot in robots.txt as of 2024-10-22.
+REQUEST_HEADERS = {"User-Agent": "AhrefsBot"}  # Observed to be approved in robots.txt as of 2024-10-22.
 SOURCE_SITE_NAME = "MedicalXpress"
 SOURCE_TYPE = "medical"
 UNBLOCK_MESSAGE = "Consider visiting https://medicalxpress.com/ in a web browser to test and unblock it as relevant."
--- src/newssurvey/sources/physorg/_common.py
+++ src/newssurvey/sources/physorg/_common.py
@@ -1,8 +1,9 @@
-from newssurvey.config import REQUEST_HEADERS
+# from newssurvey.config import REQUEST_HEADERS
 from newssurvey.util.threading_ import CooldownLock
 
-_REQUEST_COOLDOWN = 5  # Applicable to default Mozilla Firefox user agent. Values ≤2 led to HTTP 429 errors.
-REQUEST_HEADERS = REQUEST_HEADERS.copy()
+# _REQUEST_COOLDOWN = 5  # Applicable to default Mozilla Firefox user agent. Values ≤2 led to HTTP 429 errors.
+_REQUEST_COOLDOWN = 3  # Exceeds observed value of 2 for AhrefsBot in robots.txt as of 2024-10-22.
+REQUEST_HEADERS = {"User-Agent": "AhrefsBot"}  # Observed to be approved in robots.txt as of 2024-10-22.
 SOURCE_SITE_NAME = "PhysOrg"
 SOURCE_TYPE = "science"
 UNBLOCK_MESSAGE = "Consider visiting https://phys.org/ in a web browser to test and unblock it as relevant."
