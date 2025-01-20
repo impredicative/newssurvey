44c00a57101de62bb7cf42927995f1a53600a2e5
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2025-01-20 23:15:57 +0000

    Update source medicalxpress to increase _REQUEST_COOLDOWN from 3 to 4 to prevent HTTP 429 errors for AhrefsBot

--- src/newssurvey/sources/medicalxpress/_common.py
+++ src/newssurvey/sources/medicalxpress/_common.py
@@ -2,7 +2,7 @@
 from newssurvey.util.threading_ import CooldownLock
 
 # _REQUEST_COOLDOWN = 5  # Applicable to default Mozilla Firefox user agent. Values ≤4 led to HTTP 422 and 429 errors, also requiring subsequent "press-and-hold" "security verification" via a web browser.
-_REQUEST_COOLDOWN = 3  # Exceeds observed value of 2 for AhrefsBot in robots.txt as of 2024-10-22.
+_REQUEST_COOLDOWN = 4  # Exceeds observed value of 2 for AhrefsBot in robots.txt as of 2024-10-22. Value of 3 led to HTTP 429 error.
 REQUEST_HEADERS = {"User-Agent": "AhrefsBot"}  # Observed to be approved in robots.txt as of 2024-10-22.
 SOURCE_SITE_NAME = "MedicalXpress"
 SOURCE_TYPE = "medical"
